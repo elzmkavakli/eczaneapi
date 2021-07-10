60e6580716ea337fae4ddf59ef575245464001fc
elzmkavakli <elzmkavakli@gmail.com>
2021-07-10 20:02:17 +0000

    elzmkavakli-1.yukleme

---
+++ eczane.py
@@ -0,0 +1,34 @@
+import http.client
+conn = http.client.HTTPSConnection("api.collectapi.com")
+import json
+headers = {
+    'content-type': "application/json",
+    'authorization': "apikey 5tCBxAFxdnYeuY5eSZMpKT:2g7n15R9k4ootHADsjl6Vz"
+    }
+//il ve ilce ayarlamasi yapiliyor
+il=input("İl Giriniz:")
+if il=="":
+    print("İl Boş bırakılırsa Elazig İline ait veriler gelecek")
+    il="Elazig"
+else:
+    il=il.strip().capitalize()
+ilce=input("İlçe giriniz")
+if ilce=="":
+    print("İl Boş bırakılırsa Merkez ait ilk veri gelecek")
+    ilce="Merkez"
+else:
+    ilce=ilce.strip().capitalize()
+print(il,ilce)
+bilgi="/health/dutyPharmacy?ilce="+ilce+"&il="+il
+conn.request("GET",bilgi, headers=headers)
+res = conn.getresponse()
+data = res.read()
+
+veri=data.decode("utf-8")
+json_veri=json.loads(veri)
+//print(json_veri)
+if json_veri["success"]==True:
+    bilgi1=json_veri["result"][0]
+    print("Eczane Adı:"+bilgi1["name"]+"\nAdresi:"+bilgi1["address"]+"\nTelefonu:"+bilgi1["phone"])
+else:
+    print("Başarısız")
